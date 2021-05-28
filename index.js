6cd6528255f579d3325027423b5ae5c0064f8d2a
Anubhav <iamanubhavsaini+git@gmail.com>
2021-05-28 14:45:24 +0530

    v3

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const directories = (root='/') => {
-  const content = fs.readdirSync(root)
-  const getPath = x => path.join(root, x)
-  const isDirectory = x => fs.lstatSync(getPath(x)).isDirectory()
-  return content.filter(isDirectory)
+const directories = (root = '/') => {
+    try {
+        const content = fs.readdirSync(root);
+        const getPath = x => path.join(root, x);
+        const isDirectory = x => fs.lstatSync(getPath(x)).isDirectory();
+        return content.filter(isDirectory);
+    } catch (e) {
+        console.error('ERROR', e);
+        return [];
+    }
 };
 
-module.exports = directories
+module.exports = directories;
